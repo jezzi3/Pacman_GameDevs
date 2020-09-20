a630b7b718086cecdf2439d0b7936a694780154b
Will <william.w_@hotmail.com>
2020-09-20 18:24:42 +1000

    Progressing levelGenerator

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,15 +4,95 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    /**
+        0 – Empty (do not instantiate anything)
+        1 - Outside corner (double lined corener piece in orginal game)
+        2 - Outside wall (double line in original game)
+        3 - Inside corner (single lined corener piece in orginal game)
+        4 - Inside wall (single line in orginal game)
+        5 - Standard pellet (see Visual Assets above)
+        6 - Power pellet (see Visual Assets above)
+        7 - A t junction piece for connecting with adjoining regions
+    **/
+    public GameObject[] mapSprite;
+    GameObject newSprite;
+
+    //Level Array
+    int[,] levelMap =
+    {
+        {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
+        {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
+        {2,5,3,4,4,3,5,3,4,4,4,3,5,4},
+        {2,6,4,0,0,4,5,4,0,0,0,4,5,4},
+        {2,5,3,4,4,3,5,3,4,4,4,3,5,3},
+        {2,5,5,5,5,5,5,5,5,5,5,5,5,5},
+        {2,5,3,4,4,3,5,3,3,5,3,4,4,4},
+        {2,5,3,4,4,3,5,4,4,5,3,4,4,3},
+        {2,5,5,5,5,5,5,4,4,5,5,5,5,4},
+        {1,2,2,2,2,1,5,4,3,4,4,3,0,4},
+        {0,0,0,0,0,2,5,4,3,4,4,3,0,3},
+        {0,0,0,0,0,2,5,4,4,0,0,0,0,0},
+        {0,0,0,0,0,2,5,4,4,0,3,4,4,0},
+        {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
+        {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
+    };
+
     // Start is called before the first frame update
     void Start()
     {
+        int x, y;        
+        for(y = 0; y < levelMap.GetLength(0); y++)
+        {
+            for(x = 0; x < levelMap.GetLength(1); x++)
+            {
+                instantiateSprite(levelMap[y,x], x, y);
+            }
+        }
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void instantiateSprite(int sprite, int x, int y)
+    {
+        topLeftGenerator(sprite, x, y);
+        topRightGenerator(sprite, x, y);
+        bottomLeftGenerator(sprite, x, y);
+        bottomRightGenerator(sprite, x, y);
+    }
+
+    // Instantiates top left section of the level
+    void topLeftGenerator(int sprite, int x, int y)
+    {
+        switch(sprite)
+        {
+            case 1:
+            case 2:
+
+                break;
+            case 5:
+            case 6:
+                newSprite = Instantiate(mapSprite[sprite], new Vector2(x, -y), Quaternion.identity);
+                newSprite.transform.parent = gameObject.transform;
+                break;
+            default:
+                break;
+        }
+    }
+    // Instantiates top right section of the level
+    void topRightGenerator(int sprite, int x, int y)
+    {}
+
+    // Instantiates bottom left section of the level
+    void bottomLeftGenerator(int sprite, int x, int y)
+    {}
+
+    // Instantiates bottom right section of the level
+    void bottomRightGenerator(int sprite, int x, int y)
+    {}
 }
